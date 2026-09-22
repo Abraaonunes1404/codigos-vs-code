c35d292b8b22d3daa38ec45acda7fca21f202044
Abraao Nunes <abraao.nunes1404@gmail.com>
2026-09-22 00:45:17 -0300

    Adiciona rota api_remover_produto_cesta no urls py

--- mercadocompara/urls.py
+++ mercadocompara/urls.py
@@ -5,7 +5,8 @@ from core.views import (
     tela_home_cestia, tela_cesta_compras, tela_ranking_resultados, 
     tela_mapa_rota, api_scannear_codigo_barra, tela_scanner_camera,
     api_limpar_cesta, tela_cesta_vazia, api_verificar_alertas_preco,
-    tela_atualizar_preco_lojista, api_salvar_preco_rapido
+    tela_atualizar_preco_lojista, api_salvar_preco_rapido,
+    api_remover_produto_cesta
 )
 
 urlpatterns = [
@@ -22,7 +23,11 @@ urlpatterns = [
     path('api/comparar/', api_comparar_produto, name='api_comparar'),
     path('api/comparar-lista/', api_comparar_lista_compras, name='api_comparar_lista'),
     
+    # ROTA DE REMOÇÃO DO ITEM DA CESTA:
+    path('cesta/remover/<int:produto_id>/', api_remover_produto_cesta, name='remover_produto_cesta'),
+    
     # NOVAS ROTAS DO PAINEL DO LOJISTA:
     path('gerente-precos/', tela_atualizar_preco_lojista, name='atualizar_preco_lojista'),
     path('gerente-precos/salvar/', api_salvar_preco_rapido, name='api_salvar_preco'),
 ]
+
