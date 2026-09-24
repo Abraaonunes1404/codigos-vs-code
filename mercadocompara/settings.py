0cba2e15ccaf620e9d0672c228975dd97f3be467
Abraao Nunes <abraao.nunes1404@gmail.com>
2026-09-24 19:58:39 -0300

    Prepara conexao com PostgreSQL

--- mercadocompara/settings.py
+++ mercadocompara/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cz*+q3pb$zr)cge(7w&@swtan7-&p=dy)0ai-$b7xb4xv&-#4f'
@@ -48,10 +49,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mercadocompara.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
